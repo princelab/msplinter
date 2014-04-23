070c7dee7041a689e240580d1b58f0f975435e02
Ryan Taylor <ryanmt@byu.net>
2014-04-23 17:37:52 -0600

    finished scheme2 and modified the base code some to account for altered masses of original molecule

--- lib/rubabel/molecule/fragmentable/asms_2002_scheme2.rb
+++ lib/rubabel/molecule/fragmentable/asms_2002_scheme2.rb
@@ -69,7 +69,7 @@ module Rubabel
         end
         fragment_sets.flatten
       end
-      ::Rearrangements << def jasms_2002_2_f2_b_methanol_loss(only_uniqs=true)
+      ::Rearrangements << def jasms_2002_2_f2_b_methoxy_loss(only_uniqs=true)
         fragment_sets = []
         fragment = lambda do |*arr|
           # duplicate
@@ -78,6 +78,8 @@ module Rubabel
           nmol.delete_bond(c1, lost_carbon)
           nmol.delete_bond(oxygen, c2)
           c1.get_bond(c2).bond_order += 1
+          lost_carbon.remove_a_proton!
+          lost_carbon.remove_a_proton!
           nmol.split
         end
         self.matches("[C;R;r4][CH2;R;r4][O;R;r4][C;R;r4]", only_uniqs).each do |c1, lost_carbon, oxygen, c2|
@@ -103,6 +105,123 @@ module Rubabel
         end
         fragment_sets.flatten
       end
+      ::Rule_names << def jasms_2002_2_e1_b(only_uniqs=true)
+        fragment_sets = []
+        fragment = lambda do |*arr|
+          # duplicate
+          (nmol, (alcohol_oxygen, nitrogen, attacked_carbon, leaving_oxygen)) = self.dup_molecule(arr)
+          # fragmentation chemistry
+          nmol.delete_bond(leaving_oxygen, attacked_carbon)
+          nmol.add_bond!(alcohol_oxygen, attacked_carbon)
+          nitrogen.remove_a_proton!
+          nmol.split
+        end
+        self.matches("OCC(N)C(O)", only_uniqs).each do |alcohol_oxygen, c1, c2, nitrogen, attacked_carbon, leaving_oxygen|
+          fragment_sets << fragment.call(alcohol_oxygen, nitrogen, attacked_carbon, leaving_oxygen)
+        end
+        fragment_sets.flatten
+      end
+      ::Rule_names << def jasms_2002_2_e1_bprime(only_uniqs=true)
+        fragment_sets = []
+        fragment = lambda do |*arr|
+          # duplicate
+          (nmol, (alcohol_oxygen, attacked_carbon, carbon_linker, nitrogen)) = self.dup_molecule(arr)
+          # fragmentation chemistry
+          nmol.delete_bond(alcohol_oxygen, attacked_carbon)
+          nmol.add_bond!(nitrogen, attacked_carbon)
+          nitrogen.remove_a_proton!
+          nmol.split
+        end
+        self.matches("OCC(N)C(O)C=C", only_uniqs).each do |alcohol_oxygen, attacked_carbon, carbon_linker, nitrogen, *rest|
+          fragment_sets << fragment.call(alcohol_oxygen, attacked_carbon, carbon_linker, nitrogen)
+        end
+        fragment_sets.flatten
+      end
+      ::Rule_names << def jasms_2002_2_e2_b_formaldehyde_loss(only_uniqs=true)
+        fragment_sets = []
+        fragment = lambda do |*arr|
+          # duplicate
+          (nmol, (nitrogen, c1, c2, ester_oxygen, c3)) = self.dup_molecule(arr)
+          # fragmentation chemistry
+          c3.get_bond(c1).bond_order = 2
+          nmol.delete_bond(c1, c2)
+          nmol.delete_bond(ester_oxygen, c3)
+          c2.get_bond(ester_oxygen).bond_order = 2
+          nitrogen.remove_a_proton!
+          nmol.split
+        end
+        self.matches("NC1COC1C=C", only_uniqs).each do |nitrogen, c1, c2, ester_oxygen, c3, *rest|
+          fragment_sets << fragment.call(nitrogen, c1, c2, ester_oxygen, c3)
+        end
+        fragment_sets.flatten
+      end
+      ::Rule_names << def jasms_2002_2_e2_bprime_water_loss(only_uniqs=true)
+        fragment_sets = []
+        fragment = lambda do |*arr|
+          # duplicate
+          (nmol, (alcohol_oxygen, c1, c2, nitrogen)) = self.dup_molecule(arr)
+          # fragmentation chemistry
+          nmol.delete_bond(alcohol_oxygen, c1)
+          c1.get_bond(c2).bond_order = 2
+          nitrogen.remove_a_proton!
+          nmol.split
+        end
+        self.matches("OCC1CN1", only_uniqs).each do |alcohol_oxygen, c1, c2, c3, nitrogen|
+          fragment_sets << fragment.call(alcohol_oxygen, c1, c2, nitrogen)
+        end
+        fragment_sets.flatten
+      end
+      ::Rule_names << def jasms_2002_2_e2_bprime_heterocyclic_loss(only_uniqs=true)
+        fragment_sets = []
+        fragment = lambda do |*arr|
+          # duplicate
+          (nmol, (nitrogen, ring_carbon, alcohol_carbon, alcohol_oxygen)) = self.dup_molecule(arr)
+          # fragmentation chemistry
+          nmol.delete_bond(ring_carbon, alcohol_carbon)
+          alcohol_carbon.get_bond(alcohol_oxygen).bond_order = 2
+          nitrogen.remove_a_proton!
+          nmol.split
+        end
+        self.matches("N1CC1C(O)C=C", only_uniqs).each do |nitrogen, c1, ring_carbon, alcohol_carbon, alcohol_oxygen, *rest|
+          fragment_sets << fragment.call(nitrogen, ring_carbon, alcohol_carbon, alcohol_oxygen)
+        end
+        fragment_sets.flatten
+      end
+      ::Rule_names << def jasms_2002_2_c2a(only_uniqs=true)
+        fragment_sets = []
+        fragment = lambda do |*arr|
+          # duplicate
+          (nmol, (nitrogen, carbonyl_carbon, carbonyl_oxygen, alcohol_carbon, alcohol_oxygen)) = self.dup_molecule(arr)
+          # fragmentation chemistry
+          nmol.delete_bond(nitrogen, carbonyl_carbon)
+          nmol.delete_bond(carbonyl_carbon, alcohol_carbon)
+          alcohol_carbon.get_bond(alcohol_oxygen).bond_order = 2
+          nmol.split
+        end
+        self.matches("[N;R;r3]C(=O)C(O)C", only_uniqs).each do |nitrogen, carbonyl_carbon, carbonyl_oxygen, alcohol_carbon, alcohol_oxygen, *rest|
+          fragment_sets << fragment.call(nitrogen, carbonyl_carbon, carbonyl_oxygen, alcohol_carbon, alcohol_oxygen)
+        end
+        fragment_sets.flatten
+      end
+      ::Rule_names << def jasms_2002_2_a1(only_uniqs=true)
+        fragment_sets = []
+        fragment = lambda do |*arr|
+          # duplicate
+          (nmol, (base_c, alcohol_carbon, alcohol_oxygen, carbonyl_carbon, carbonyl_oxygen, nitrogen, ring_carbon)) = self.dup_molecule(arr)
+          # fragmentation chemistry
+          nitrogen.get_bond(carbonyl_carbon).bond_order = 2
+          nmol.delete_bond(carbonyl_carbon, alcohol_carbon)
+          alcohol_carbon.get_bond(alcohol_oxygen).bond_order = 2
+          nmol.delete_bond(nitrogen, ring_carbon)
+          nmol.split
+        end
+        self.matches("CC(O)C(=O)N[C;R;r4]", only_uniqs).each do |base_c, alcohol_carbon, alcohol_oxygen, carbonyl_carbon, carbonyl_oxygen, nitrogen, ring_carbon|
+          fragment_sets << fragment.call(base_c, alcohol_carbon, alcohol_oxygen, carbonyl_carbon, carbonyl_oxygen, nitrogen, ring_carbon)
+        end
+        fragment_sets.flatten
+      end
+
+
     end # Fragmentable
   end # Molecule
 end #Rubabel
--- spec/rubabel/molecule/asms_2002_scheme2_spec.rb
+++ spec/rubabel/molecule/asms_2002_scheme2_spec.rb
@@ -11,39 +11,94 @@ describe Rubabel::Molecule::Fragmentable do
     # Comment here
     specify 'rule: jasms_2002_2_f1' do
       mol = Rubabel["OCC(NC(=O)C(O)C)C(O)C=C"]
+      wt = mol.mol_wt
       frags = mol.fragment(rules: [:jasms_2002_2_f1], rearrange: false)
       frags.map{|a| a.map(&:csmiles) }.flatten.should == ["CC=O", "O=CNC(C(C=C)O)CO"].reverse
-      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(mol.mol_wt,MASS_DELTA)
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
     end
     specify 'rule: jasms_2002_2_f1_aprime' do
       mol = Rubabel["O=CNC(C(C=C)O)CO"]
+      wt = mol.mol_wt
       frags = mol.fragment(rules: [:jasms_2002_2_f1_aprime], rearrange: false)
       frags.map {|a| a.map(&:csmiles)}.flatten.should == ["O=CNC1COC1C=C", "O"]
-      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(mol.mol_wt,MASS_DELTA)
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
     end
     specify 'rule: jasms_2002_2_f1_bprime' do
       mol = Rubabel["O=CNC(C(C=C)O)CO"]
+      wt = mol.mol_wt
       frags = mol.fragment(rules: [:jasms_2002_2_f1_bprime], rearrange: false)
       frags.map {|a| a.map(&:csmiles)}.flatten.should == ["C=CC(C1CN1C=O)O", "O"]
-      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(mol.mol_wt,MASS_DELTA)
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
     end
-    specify 'rearrangement: jasms_2002_2_f2_b_methanol_loss' do
+    specify 'rearrangement: jasms_2002_2_f2_b_methoxy_loss' do
       mol = Rubabel["O=CNC1COC1C=C"]
-      frags = mol.rearrange(rules: [:jasms_2002_2_f2_b_methanol_loss])
-      frags.map {|a| a.map(&:csmiles)}.flatten.should == ["CO", "O=CNC=CC=C"].reverse
-      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(mol.mol_wt,MASS_DELTA)
+      wt = mol.mol_wt
+      frags = mol.rearrange(rules: [:jasms_2002_2_f2_b_methoxy_loss])
+      frags.map {|a| a.map(&:csmiles)}.flatten.should == ["[CH2-][OH]", "O=CNC=CC=C"].reverse
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
     end
     specify 'rearrangement: jasms_2002_2_f2_bprime_water_loss' do
       mol = Rubabel["O=CN1CC1C(O)C=C"]
+      wt = mol.mol_wt
       frags = mol.rearrange(rules: [:jasms_2002_2_f2_bprime_water_loss])
       frags.map {|a| a.map(&:csmiles)}.flatten.should == ["O", "C=CC=C1CN1C=O"].reverse
-      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(mol.mol_wt,MASS_DELTA)
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
     end
     specify 'rule: jasms_2002_2_e1' do
       mol = Rubabel["OCC(NC(=O)C(O)C)C(O)C=C"]
+      wt = mol.mol_wt
       frags = mol.fragment(rules: [:jasms_2002_2_e1], rearrange: false)
-      frags.map {|a| a.map(&:csmiles)}.flatten.should == ["CC=O", "[O-]#C", "OCC(C(C=C)O)N"].reverse # Should be a C#O without any charge
-      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(mol.mol_wt,MASS_DELTA)
+      frags.map {|a| a.map(&:csmiles)}.flatten.should == ["CC=O", "[O-]#C", "OCC(C(C=C)O)N"].reverse # Should be a C#O without any charge, although I don't find spectral evidence for this ion
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
+    end
+    specify 'rule: jasms_2002_2_e1_b' do 
+      mol = Rubabel["OCC(C(C=C)O)N"]
+      wt = mol.mol_wt
+      frags = mol.fragment(rules: [:jasms_2002_2_e1_b], rearrange: false)
+      frags.map {|a| a.map(&:csmiles)}.flatten.should == ["O", "NC1COC1C=C"].reverse
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
+    end
+    specify 'rule: jasms_2002_2_e1_bprime' do 
+      mol = Rubabel["OCC(C(C=C)O)N"]
+      wt = mol.mol_wt
+      frags = mol.fragment(rules: [:jasms_2002_2_e1_bprime], rearrange: false)
+      frags.map {|a| a.map(&:csmiles)}.flatten.should == ["O", "OC(C1CN1)C=C"]
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
+    end
+    specify 'rearrangement: jasms_2002_2_e2_b_formaldehyde_loss' do 
+      mol = Rubabel["NC1COC1C=C"]
+      wt = mol.mol_wt
+      frags = mol.rearrange(rules: [:jasms_2002_2_e2_b_formaldehyde_loss])
+      frags.map {|a| a.map(&:csmiles)}.flatten.should == ["C=O", "NC=CC=C"].reverse
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
+    end
+    specify 'rearrangement: jasms_2002_2_e2_bprime_water_loss' do 
+      mol = Rubabel["N1CC1C(O)C=C"]
+      wt = mol.mol_wt
+      frags = mol.rearrange(rules: [:jasms_2002_2_e2_bprime_water_loss])
+      frags.map {|a| a.map(&:csmiles)}.flatten.should == ["O", "C=CC=C1CN1"].reverse
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
+    end
+    specify 'rule: jasms_2002_2_e2_bprime_heterocyclic_loss' do 
+      mol = Rubabel["N1CC1C(O)C=C"]
+      wt = mol.mol_wt
+      frags = mol.fragment(rules: [:jasms_2002_2_e2_bprime_heterocyclic_loss], rearrange: false)
+      frags.map {|a| a.map(&:csmiles)}.flatten.should == ["N1CC1", "C=CC=O"]
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
+    end
+    specify 'rule: jasms_2002_2_c2a' do 
+      mol = Rubabel["C1CN1C(=O)C(O)C"]
+      wt = mol.mol_wt
+      frags = mol.fragment(rules: [:jasms_2002_2_c2a], rearrange: false)
+      frags.map {|a| a.map(&:csmiles)}.flatten.should == ["CC=O","C=O", "N1CC1"].reverse
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
+    end
+    specify 'rule: jasms_2002_2_a1' do 
+      mol = Rubabel["CC(O)C(=O)NC1COC1C=C"]
+      wt = mol.mol_wt
+      frags = mol.fragment(rules: [:jasms_2002_2_a1], rearrange: false)
+      frags.map {|a| a.map(&:csmiles)}.flatten.should == ["CC=O", "N=C=O","C=CC1CCO1"]
+      frags.map {|a| a.map(&:mol_wt)}.flatten.inject(:+).should be_close(wt,MASS_DELTA)
     end
   end
 end
